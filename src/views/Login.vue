ddb9dc60eb4f7d80e21412325850366ac55fb2d6
prodavies <39768603+prodavies@users.noreply.github.com>
2021-08-01 23:27:01 +0300

    another changes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,31 +2,16 @@
    <v-app>
    <v-main>
      <v-container>
-       <v-form
-          ref="form"
-          lazy-validation
-        >
-          <v-text-field
-            v-model="email"
-            label="E-mail"
-            required
-          ></v-text-field>
+       <v-form ref="form" lazy-validation>
+          <v-text-field v-model="email" label="E-mail" required></v-text-field>
 
-          <v-text-field
-            v-model="password"
-            type="password"
-            label="Password"
-            required
-          ></v-text-field>
+          <v-text-field v-model="password" type="password" label="Password" required></v-text-field>
 
-          <v-btn
-            color="primary"
-            @click="loginUser"
-          >
+          <v-btn color="primary" @click="loginUser">
            Login
           </v-btn>
 
-           <span class="ml-4">Don't have account? | </span> <router-link to="/signup">sign up</router-link>
+           <!-- <span class="ml-4">Don't have account? | </span> <router-link to="/signup">sign up</router-link> -->
         </v-form>
      </v-container>
    </v-main>
